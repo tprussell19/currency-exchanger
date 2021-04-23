16128a80a946e1649d031356a48e91fd908802d1
Thomas Russell <t.p.russell19@gmail.com>
2021-04-23 13:12:51 -0700

    BUGFIX: change dependency versions and re-install all to ensure gfunctionality

--- src/main.js
+++ src/main.js
@@ -1,29 +1,26 @@
-import $ from 'jquery'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './css/styles.css'
-import ExchangeService from './js/exchange-service.js'
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import ExchangeService from './js/exchange-service.js';
 
 function showMeTheMoney(response) {
   if (response) {
     // let inputAmount = $('input-money').val();
     // let newCurrency = $('select-exchange').val();
     // console.log('amount: ', inputAmount, 'currency selected: ', newCurrency)
-    console.log(response);
     $('#returned-money').html(response.conversion_rates.AUD);
   } else {
-    console.log('the response failed')
+    console.log('the response failed');
   }
 }
 
 $(document).ready(function() {
-  console.log(API_KEY)
   $('form').submit(function(event) {
     event.preventDefault();
     ExchangeService.getNewMoney()
-    .then(function(response) {
-      console.log(response);
-      showMeTheMoney(response);
+      .then(function(response) {
+        showMeTheMoney(response);
       });
   });
 });
