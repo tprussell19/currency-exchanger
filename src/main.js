a1f15fe866d9d12283d6587c4ce8f600fbcf4f83
Thomas Russell <t.p.russell19@gmail.com>
2021-04-23 17:46:13 -0700

    add currecny symbols to outputs, show/hide errors and outputs

--- src/main.js
+++ src/main.js
@@ -6,34 +6,45 @@ import ExchangeService from './js/exchange-service.js';
 
 function showMeTheMoney(response) {
   if (response.result === "success") {
-    console.log(response.result);
     let newCurrency = $('#select-exchange').val();
     let inputAmount = parseFloat($('#input-money').val());
     $('#input-money').val("");
     let outputAmount;
-    console.log(newCurrency);
-    if (newCurrency === "EUR") {
-      outputAmount = (inputAmount * response.conversion_rates.EUR).toFixed(2);
-      $('#returned-money').html(`${outputAmount}`);
-    } else if (newCurrency === "CNY") {
-      outputAmount = (inputAmount * response.conversion_rates.CNY).toFixed(2);
-      $('#returned-money').html(`${outputAmount}`);
-    } else if (newCurrency === "NGN") {
-      outputAmount = (inputAmount * response.conversion_rates.NGN).toFixed(2);
-      $('#returned-money').html(`${outputAmount}`);
-    } else if (newCurrency === "BRL") {
-      outputAmount = (inputAmount * response.conversion_rates.BRL).toFixed(2);
-      $('#returned-money').html(`${outputAmount}`);
-    } else if (newCurrency === "AUD") {
-      outputAmount = (inputAmount * response.conversion_rates.AUD).toFixed(2);
-      $('#returned-money').html(`${outputAmount}`);
+    console.log(inputAmount);
+    if (isNaN(inputAmount)) {
+      $('#returned-money').hide();
+      $('#api-error').hide();
+      $('#show-error').show();
+      $('#show-error').html("Please enter a valid number");
+    } else {
+      $('#api-error').hide();
+      $('#show-error').hide();
+      $('#returned-money').show();
+      if (newCurrency === "EUR") {
+        outputAmount = (inputAmount * response.conversion_rates.EUR).toFixed(2);
+        $('#returned-money').html(`€${outputAmount}`);
+      } else if (newCurrency === "CNY") {
+        outputAmount = (inputAmount * response.conversion_rates.CNY).toFixed(2);
+        $('#returned-money').html(`¥${outputAmount}`);
+      } else if (newCurrency === "NGN") {
+        outputAmount = (inputAmount * response.conversion_rates.NGN).toFixed(2);
+        $('#returned-money').html(`₦${outputAmount}`);
+      } else if (newCurrency === "BRL") {
+        outputAmount = (inputAmount * response.conversion_rates.BRL).toFixed(2);
+        $('#returned-money').html(`R$${outputAmount}`);
+      } else if (newCurrency === "AUD") {
+        outputAmount = (inputAmount * response.conversion_rates.AUD).toFixed(2);
+        $('#returned-money').html(`$${outputAmount}`);
+      }
     }
   } else {
-    console.log(error, response["error-type"]);
+    $('#show-error').hide();
+    $('#returned-money').hide();
+    $('#api-error').show();
     if (response.result === "error") {
       $('#api-error').html(`This is an error: ${response["error-type"]}`);
     } else {
-      $('#api-error').html(`This is an error: ${response}`);
+      $('#api-error').html(`This is an error: ${error.message} ${response}`);
     }
   }
 }
